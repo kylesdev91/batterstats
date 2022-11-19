9a21f76db324db0d0111139b014501638036dc0d
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-19 10:30:24 -0600

    fixed team and player component

--- src/components/CreatePlayer.vue
+++ src/components/CreatePlayer.vue
@@ -13,7 +13,7 @@ const sort = ref(false);
 
 const CreateUser = () => {
   if (!user_input.value.player) {
-    return alert("Please enter a player");
+    return alert("Please enter a Player");
   }
   user_store.create(user_input.value);
   user_input.value = {
@@ -29,32 +29,58 @@ const DeletePlayer = (id) => {
 <template>
   <main>
     <form @submit.prevent="CreateUser">
-      <v-responsive class="mx-auto" max-width="344">
-        <h2 class="text-center">Player</h2>
-        <v-text-field
-          hide-details="auto"
-          label="Player"
-          v-model="user_input.player"
-          class="my-2"
-        ></v-text-field>
-       </v-responsive>
-       <div class="text-center">
-        <v-btn class="mx-auto" color="blue" variant="tonal" type="submit" value="Create">Create</v-btn>
-       </div>
+      <h2 class="text-center">Player</h2>
+
+      <v-row class="my-10">
+        <v-col cols="6">
+          <v-text-field
+            hide-details="auto"
+            label="Player"
+            v-model="user_input.player"
+          ></v-text-field>
+        </v-col>
+        <v-row align="center">
+          <v-btn
+            class="mx-4"
+            color="blue"
+            variant="tonal"
+            type="submit"
+            value="Create"
+            >Create</v-btn
+          >
+        </v-row>
+      </v-row>
     </form>
+    <v-simple-table fixed-header fill-height="300px" class="my-15">
+      <thead>
+        <tr>
+          <th><p>Player</p></th>
+        </tr>
+      </thead>
+    </v-simple-table>
 
     <div class="users" v-if="!sort">
       <div v-for="user in user_store.users" class="user">
-        <h3 class="text-center">{{ user.player }}</h3>
+        <h2 class="text-left">{{ user.player }}</h2>
         <div class="text-center">
-             <v-btn class="text-right" color="red" variant="tonal" @click="DeletePlayer(user.id)">Delete</v-btn>
-        </div>
+          <v-row></v-row>
+          <v-col cols="12" sm="12" class="mx-12">
+            <v-btn
+              class="delete"
+              color="red"
+              variant="tonal"
+              value="Delete"
+              @click="DeletePlayer(user.id)"
+              >Delete</v-btn
+            >
+          </v-col>
         </div>
       </div>
+    </div>
 
     <div class="users" v-else>
       <div v-for="user in user_store.usersByName" class="user">
-        <h3>{{ user.player }}</h3>
+         <h3>{{ user.player }}</h3>
       </div>
     </div>
   </main>
--- src/components/CreateTeam.vue
+++ src/components/CreateTeam.vue
@@ -29,32 +29,58 @@ const DeleteTeam = (id) => {
 <template>
   <main>
     <form @submit.prevent="CreateUser">
-      <v-responsive class="mx-auto" max-width="344">
-        <h2 class="text-center">Team</h2>
-        <v-text-field
-          hide-details="auto"
-          label="Team"
-          v-model="user_input.team"
-          class="my-2"
-        ></v-text-field>
-       </v-responsive>
-       <div class="text-center">
-        <v-btn class="mx-auto" color="blue" variant="tonal" type="submit" value="Create">Create</v-btn>
-       </div>
+      <h2 class="text-center">Team</h2>
+
+      <v-row class="my-10">
+        <v-col cols="6">
+          <v-text-field
+            hide-details="auto"
+            label="Team"
+            v-model="user_input.team"
+          ></v-text-field>
+        </v-col>
+        <v-row align="center">
+          <v-btn
+            class="mx-4"
+            color="blue"
+            variant="tonal"
+            type="submit"
+            value="Create"
+            >Create</v-btn
+          >
+        </v-row>
+      </v-row>
     </form>
+    <v-simple-table fixed-header fill-height="300px" class="my-15">
+      <thead>
+        <tr>
+          <th><p>Team</p></th>
+        </tr>
+      </thead>
+    </v-simple-table>
 
     <div class="users" v-if="!sort">
       <div v-for="user in user_store.users" class="user">
-        <h3 class="text-center">{{ user.team }}</h3>
+        <h2 class="text-left">{{ user.team }}</h2>
         <div class="text-center">
-             <v-btn class="text-right" color="red" variant="tonal" @click="DeleteTeam(user.id)">Delete</v-btn>
-        </div>
+          <v-row></v-row>
+          <v-col cols="12" sm="12" class="mx-12">
+            <v-btn
+              class="delete"
+              color="red"
+              variant="tonal"
+              value="Delete"
+              @click="DeleteTeam(user.id)"
+              >Delete</v-btn
+            >
+          </v-col>
         </div>
       </div>
+    </div>
 
     <div class="users" v-else>
       <div v-for="user in user_store.usersByName" class="user">
-        <h3>{{ user.team }}</h3>
+         <h3>{{ user.team }}</h3>
       </div>
     </div>
   </main>
