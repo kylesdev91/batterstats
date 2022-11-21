4223ab859c6e552d8b3f5ea080095c1ddbb69e8d
Eric Hernandez <erichernandez@Erics-MBP.home>
2022-11-20 21:30:18 -0600

    add Kyle

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CreateTeam from "../components/CreateTeam.vue";
 import CreatePlayer from "../components/CreatePlayer.vue";
+import KyleABC from "../components/KyleABC.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/createplayer",
       name: "createplayer",
       component: CreatePlayer,
+    },
+    {
+      path: "/kyleabc",
+      name: "kyleabc",
+      component: KyleABC,
     },
     {
       path: "/about",
